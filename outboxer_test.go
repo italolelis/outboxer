b8942cd2460863a09efa093c1116660c0f993b1b
Italo Vietro <italolelis@gmail.com>
2019-02-02 15:14:38 +0100

    Not an integration test anymore

--- outboxer_test.go
+++ outboxer_test.go
@@ -2,45 +2,78 @@ package outboxer_test
 
 import (
 	"context"
-	"database/sql"
-	"os"
+	"errors"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/italolelis/outboxer"
 
 	amqpOut "github.com/italolelis/outboxer/amqp"
-	"github.com/italolelis/outboxer/postgres"
 	_ "github.com/lib/pq"
-	"github.com/streadway/amqp"
 )
+
+type inMemDS struct {
+	data []*outboxer.OutboxMessage
+}
+
+func (inmem *inMemDS) GetEvents(ctx context.Context, batchSize int32) ([]*outboxer.OutboxMessage, error) {
+	return inmem.data, nil
+}
+
+func (inmem *inMemDS) SetAsDispatched(ctx context.Context, id int64) error {
+	for _, m := range inmem.data {
+		if m.ID == id {
+			m.Dispatched = true
+			m.DispatchedAt.Time = time.Now()
+			return nil
+		}
+	}
+
+	return fmt.Errorf("message not found")
+}
+
+func (inmem *inMemDS) Add(ctx context.Context, m *outboxer.OutboxMessage) error {
+	inmem.data = append(inmem.data, m)
+
+	return nil
+}
+
+func (inmem *inMemDS) AddWithinTx(ctx context.Context, m *outboxer.OutboxMessage, fn func(outboxer.ExecerContext) error) error {
+	return inmem.Add(ctx, m)
+}
+
+func (inmem *inMemDS) Remove(ctx context.Context, cleanUpBefore time.Time, batchSize int32) error {
+	for i, m := range inmem.data {
+		if m.DispatchedAt.Time == cleanUpBefore {
+			inmem.data = append(inmem.data[:i], inmem.data[i+1:]...)
+			return nil
+		}
+	}
+
+	return errors.New("event not found")
+}
+
+type inMemES struct {
+	ok bool
+}
+
+// Send mocks the behaviour of the event store
+func (inmem *inMemES) Send(context.Context, *outboxer.OutboxMessage) error {
+	if inmem.ok {
+		return nil
+	}
+
+	return errors.New("mock returned an error")
+}
 
 func TestIntegrationOutboxer(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	db, err := sql.Open("postgres", os.Getenv("DS_DSN"))
-	if err != nil {
-		t.Fatalf("could not connect to amqp: %s", err)
-	}
-
-	conn, err := amqp.Dial(os.Getenv("ES_DSN"))
-	if err != nil {
-		t.Fatalf("could not connect to amqp: %s", err)
-	}
-
-	t.Log("created postgres datastore")
-	ds, err := postgres.WithInstance(ctx, db)
-	if err != nil {
-		t.Fatalf("could not setup the data store: %s", err)
-	}
-	defer ds.Close()
-
-	t.Log("created amqp event stream")
-	es := amqpOut.NewAMQP(conn)
 	o, err := outboxer.New(
-		outboxer.WithDataStore(ds),
-		outboxer.WithEventStream(es),
+		outboxer.WithDataStore(&inMemDS{}),
+		outboxer.WithEventStream(&inMemES{true}),
 		outboxer.WithCheckInterval(1*time.Second),
 		outboxer.WithCleanupInterval(5*time.Second),
 		outboxer.WithCleanUpBefore(time.Now().AddDate(0, 0, -5)),
@@ -87,5 +120,4 @@ func TestIntegrationOutboxer(t *testing.T) {
 
 	t.Log("waiting for succesfully sent messages...")
 	<-done
-	t.Log("messages received")
 }
