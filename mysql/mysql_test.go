b04d9d7be2505150252b24aee096dc0d3398c5b0
Italo Vietro <italo.vietro@n26.com>
2019-02-14 18:59:26 +0100

    Mocking the mysql
    Signed-off-by: Italo Vietro <italo.vietro@n26.com>

--- mysql/mysql_test.go
+++ mysql/mysql_test.go
@@ -2,59 +2,26 @@ package mysql
 
 import (
 	"context"
-	"database/sql"
-	"github.com/go-sql-driver/mysql"
-	"net/url"
-	"os"
-	"strings"
+	"errors"
 	"testing"
 	"time"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/italolelis/outboxer"
+	"github.com/italolelis/outboxer/lock"
 )
 
-func TestMySQL_Datastore(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		scenario string
-		function func(context.Context, *testing.T, outboxer.DataStore)
-	}{
-		{
-			"successfully add the message into the event store",
-			testAddSuccessfully,
-		},
-		{
-			"successfully set the message as dispatched",
-			testSetDispatchedSuccessfully,
-		},
-		{
-			"add messages within a transaction",
-			testAddTx,
-		},
-	}
-
+func TestMySQL_AddSuccessfully(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	u, err := url.Parse(os.Getenv("MYSQL_DS_DSN"))
+	db, mock, err := sqlmock.New()
 	if err != nil {
-		t.Fatalf("failed to parse MYSQL DSN: %s", err)
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
+	defer db.Close()
 
-	c, err := urlToMySQLConfig(*u)
-	if err != nil {
-		t.Fatalf("failed parse the DSN into a MYSQL Config: %s", err)
-	}
-
-	db, err := sql.Open("mysql", c.FormatDSN())
-	if err != nil {
-		t.Fatalf("failed to connect to mysql: %s", err)
-	}
-
-	db.SetMaxIdleConns(0)
-	db.SetConnMaxLifetime(time.Second * 60)
+	initDatastoreMock(t, mock)
 
 	ds, err := WithInstance(ctx, db)
 	if err != nil {
@@ -62,18 +29,24 @@ func TestMySQL_Datastore(t *testing.T) {
 	}
 	defer ds.Close()
 
-	if err := ds.Remove(ctx, time.Now(), 20); err != nil {
-		t.Fatalf("failed to clean the data store before starting tests for mysql: %s", err)
-	}
+	mock.ExpectBegin()
+	mock.ExpectExec(`INSERT INTO event_store (.+) VALUES (.+)`).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mock.ExpectCommit()
 
-	for _, test := range tests {
-		t.Run(test.scenario, func(t *testing.T) {
-			test.function(ctx, t, ds)
-		})
-	}
-}
+	mock.ExpectQuery(`SELECT (.+) FROM event_store WHERE dispatched = false LIMIT 10`).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "dispatched", "dispatched_at", "payload", "options", "headers"}).
+			AddRow(1, false, time.Now(), []byte("test payload"), outboxer.DynamicValues{}, outboxer.DynamicValues{}))
 
-func testAddSuccessfully(ctx context.Context, t *testing.T, ds outboxer.DataStore) {
+	mock.ExpectExec(`update event_store set (.+)where id = ?`).
+		WithArgs(1).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+
+	mock.ExpectBegin()
+	mock.ExpectExec(`DELETE FROM (.+) WHERE (.+) LIMIT 10`).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mock.ExpectCommit()
+
 	if err := ds.Add(ctx, &outboxer.OutboxMessage{
 		Payload: []byte("test payload"),
 	}); err != nil {
@@ -101,31 +74,70 @@ func testAddSuccessfully(ctx context.Context, t *testing.T, ds outboxer.DataStor
 	}
 }
 
-func testSetDispatchedSuccessfully(ctx context.Context, t *testing.T, ds outboxer.DataStore) {
-	if err := ds.Add(ctx, &outboxer.OutboxMessage{
-		Payload: []byte("test payload"),
-	}); err != nil {
-		t.Fatalf("failed to add message in the data store: %s", err)
-	}
+func TestMySQL_WithInstanceNoDB(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	msgs, err := ds.GetEvents(ctx, 10)
+	db, mock, err := sqlmock.New()
 	if err != nil {
-		t.Fatalf("failed to retrieve messages from the data store: %s", err)
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
+	defer db.Close()
 
-	for _, m := range msgs {
-		err := ds.SetAsDispatched(ctx, m.ID)
-		if err != nil {
-			t.Fatalf("failed to set message as dispatched: %s", err)
-		}
-	}
+	mock.ExpectQuery(`SELECT DATABASE()`).
+		WillReturnError(errors.New("missing database"))
 
-	if err := ds.Remove(ctx, time.Now(), 10); err != nil {
-		t.Fatalf("failed to remove messages: %s", err)
+	_, err = WithInstance(ctx, db)
+	if err == nil {
+		t.Fatal("setting up the DS instance is supposed to error", err)
 	}
 }
 
-func testAddTx(ctx context.Context, t *testing.T, ds outboxer.DataStore) {
+func TestMySQL_WithInstanceWithEmptyDBName(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery(`SELECT DATABASE()`).
+		WillReturnRows(sqlmock.NewRows([]string{"DATABASE()"}).AddRow(""))
+
+	_, err = WithInstance(ctx, db)
+	if err == nil {
+		t.Fatal("setting up the DS instance is supposed to error", err)
+	}
+}
+
+func TestMySQL_AddWithinTx(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectQuery(`SELECT DATABASE()`).
+		WillReturnRows(sqlmock.NewRows([]string{"DATABASE()"}).AddRow("test"))
+
+	ds, err := WithInstance(ctx, db)
+	if err != nil {
+		t.Fatalf("failed to setup the data store: %s", err)
+	}
+	defer ds.Close()
+
+	mock.ExpectBegin()
+	mock.ExpectExec(`SELECT (.+) from event_store LIMIT 1`).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mock.ExpectExec(`INSERT INTO event_store (.+) VALUES (.+)`).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mock.ExpectCommit()
+
 	fn := func(tx outboxer.ExecerContext) error {
 		_, err := tx.ExecContext(ctx, "SELECT * from event_store LIMIT 1")
 		return err
@@ -136,39 +148,22 @@ func testAddTx(ctx context.Context, t *testing.T, ds outboxer.DataStore) {
 	}, fn); err != nil {
 		t.Fatalf("failed to add message in the data store: %s", err)
 	}
-
-	msgs, err := ds.GetEvents(ctx, 10)
-	if err != nil {
-		t.Fatalf("failed to retrieve messages from the data store: %s", err)
-	}
-
-	for _, m := range msgs {
-		err := ds.SetAsDispatched(ctx, m.ID)
-		if err != nil {
-			t.Fatalf("failed to set message as dispatched: %s", err)
-		}
-	}
-
-	if err := ds.Remove(ctx, time.Now(), 10); err != nil {
-		t.Fatalf("failed to remove messages: %s", err)
-	}
 }
 
-// urlToMySQLConfig takes a net/url URL and returns a go-sql-driver/mysql Config.
-// Manually sets username and password to avoid net/url from url-encoding the reserved URL characters
-func urlToMySQLConfig(u url.URL) (*mysql.Config, error) {
-	origUserInfo := u.User
-	u.User = nil
-
-	c, err := mysql.ParseDSN(strings.TrimPrefix(u.String(), "mysql://"))
+func initDatastoreMock(t *testing.T, mock sqlmock.Sqlmock) {
+	aid, err := lock.Generate("test", "event_store")
 	if err != nil {
-		return nil, err
+		t.Fatalf("failed to generate the lock value: %s", err)
 	}
-	if origUserInfo != nil {
-		c.User = origUserInfo.Username()
-		if p, ok := origUserInfo.Password(); ok {
-			c.Passwd = p
-		}
-	}
-	return c, nil
+
+	mock.ExpectQuery(`SELECT DATABASE()`).
+		WillReturnRows(sqlmock.NewRows([]string{"DATABASE()"}).AddRow("test"))
+	mock.ExpectQuery(`SELECT GET_LOCK(.+)`).
+		WithArgs(aid).
+		WillReturnRows(sqlmock.NewRows([]string{"GET_LOCK"}).AddRow(true))
+	mock.ExpectExec(`CREATE TABLE IF NOT EXISTS event_store (.+) ENGINE=InnoDB DEFAULT CHARSET=utf8;`).
+		WillReturnResult(sqlmock.NewResult(0, 1))
+	mock.ExpectExec(`SELECT RELEASE_LOCK(.+)`).
+		WithArgs(aid).
+		WillReturnResult(sqlmock.NewResult(0, 1))
 }
