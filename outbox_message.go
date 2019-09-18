a149d2df0bed5aab3c96e73f45a53b2402ad378e
italolelis <italo.vietro@lykon.com>
2019-09-18 10:55:24 +0200

    Use the new go 1.13 sql.NullTime

--- outbox_message.go
+++ outbox_message.go
@@ -1,41 +1,21 @@
 package outboxer
 
 import (
+	"database/sql"
 	"database/sql/driver"
 	"encoding/json"
 	"fmt"
 	"reflect"
-	"time"
 )
 
 // OutboxMessage represents a message that will be sent
 type OutboxMessage struct {
 	ID           int64
 	Dispatched   bool
-	DispatchedAt NullTime
+	DispatchedAt sql.NullTime
 	Payload      []byte
 	Options      DynamicValues
 	Headers      DynamicValues
-}
-
-// NullTime represents a nullable time.Time
-type NullTime struct {
-	Time  time.Time
-	Valid bool // Valid is true if Time is not NULL
-}
-
-// Scan implements the Scanner interface.
-func (nt *NullTime) Scan(value interface{}) error {
-	nt.Time, nt.Valid = value.(time.Time)
-	return nil
-}
-
-// Value implements the driver Valuer interface.
-func (nt NullTime) Value() (driver.Value, error) {
-	if !nt.Valid {
-		return nil, nil
-	}
-	return nt.Time, nil
 }
 
 // DynamicValues is a map that can be serialized
