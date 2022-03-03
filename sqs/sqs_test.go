d6891e988cd44453b95641bca6221c9a53519c6f
Felipe Agger <felipe_life@live.com>
2022-03-03 20:25:50 -0300

    Adjusting SQS to use Functional Options and table driven tests.

--- sqs/sqs_test.go
+++ sqs/sqs_test.go
@@ -34,80 +34,60 @@ func TestSQS_EventStream(t *testing.T) {
 		t.Fatalf("failed to setup an aws session: %s", err)
 	}
 
-	queueName := aws.String("test")
-	queueArn := aws.String(fmt.Sprintf("%v/000000000000/%v", endpoint, *queueName))
-
 	sqsClient := sqs.New(sess)
-	if _, err := sqsClient.CreateQueueWithContext(ctx, &sqs.CreateQueueInput{QueueName: queueName}); err != nil {
-		var kErr awserr.Error
-		if errors.As(err, &kErr) {
-			if kErr.Code() == "ResourceInUseException" {
-				t.Log(kErr.Message())
-			}
-		} else {
-			t.Fatalf("failed to create queue: %s", err)
-		}
+
+	tests := []struct {
+		message   string
+		queueName *string
+		attrs     map[string]*string
+		groupId   *string
+	}{
+		{message: "test payload", queueName: aws.String("test"), attrs: nil, groupId: nil},
+		{message: "test fifo payload", queueName: aws.String("test.fifo"), attrs: map[string]*string{"FifoQueue": aws.String("true")}, groupId: aws.String("123")},
 	}
 
-	es := New(sqsClient)
-	if err := es.Send(ctx, &outboxer.OutboxMessage{
-		Payload: []byte("test payload"),
-		Options: map[string]interface{}{
+	for _, tc := range tests {
+
+		queueName := tc.queueName
+		queueArn := aws.String(fmt.Sprintf("%v/000000000000/%v", endpoint, *queueName))
+
+		var err error
+		if len(tc.attrs) > 0 {
+			_, err = sqsClient.CreateQueueWithContext(ctx, &sqs.CreateQueueInput{QueueName: queueName, Attributes: tc.attrs})
+		} else {
+			_, err = sqsClient.CreateQueueWithContext(ctx, &sqs.CreateQueueInput{QueueName: queueName})
+		}
+
+		if err != nil {
+			var kErr awserr.Error
+			if errors.As(err, &kErr) {
+				if kErr.Code() == "ResourceInUseException" {
+					t.Log(kErr.Message())
+				}
+			} else {
+				t.Fatalf("failed to create queue: %s", err)
+			}
+		}
+
+		options := map[string]interface{}{
 			QueueNameOption: *queueArn,
-		},
-	}); err != nil {
-		t.Fatalf("an error was not expected: %s", err)
-	}
-}
-
-func TestSQSFIFO_EventStream(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	endpoint := os.Getenv("SQS_ENDPOINT")
-	if endpoint == "" {
-		endpoint = "http://localhost:4566"
-	}
-
-	sess, err := session.NewSession(&aws.Config{
-		CredentialsChainVerboseErrors: aws.Bool(true),
-		Credentials:                   credentials.NewStaticCredentials("foo", "var", ""),
-		Endpoint:                      aws.String(endpoint),
-		Region:                        aws.String("us-east-1"),
-	})
-	if err != nil {
-		t.Fatalf("failed to setup an aws session: %s", err)
-	}
-
-	queueName := aws.String("test.fifo")
-	queueArn := aws.String(fmt.Sprintf("%v/000000000000/%v", endpoint, *queueName))
-	groupId := aws.String("123")
-
-	attrs := map[string]*string{"FifoQueue": aws.String("true")}
-
-	sqsClient := sqs.New(sess)
-	if _, err := sqsClient.CreateQueueWithContext(ctx, &sqs.CreateQueueInput{QueueName: queueName, Attributes: attrs}); err != nil {
-		var kErr awserr.Error
-		if errors.As(err, &kErr) {
-			if kErr.Code() == "ResourceInUseException" {
-				t.Log(kErr.Message())
-			}
-		} else {
-			t.Fatalf("failed to create queue: %s", err)
 		}
+
+		if tc.groupId != nil {
+			options[MessageGroupIdOption] = *tc.groupId
+		}
+
+		es := New(sqsClient)
+		if err := es.Send(ctx, &outboxer.OutboxMessage{
+			Payload: []byte(tc.message),
+			Options: options,
+			Headers: map[string]interface{}{
+				"HeaderKey": *queueArn,
+			},
+		}); err != nil {
+			t.Fatalf("an error was not expected: %s", err)
+		}
+
 	}
 
-	es := New(sqsClient)
-	if err := es.Send(ctx, &outboxer.OutboxMessage{
-		Payload: []byte("test fifo payload"),
-		Options: map[string]interface{}{
-			QueueNameOption:      *queueArn,
-			MessageGroupIdOption: *groupId,
-		},
-		Headers: map[string]interface{}{
-			"HeaderKey": *groupId,
-		},
-	}); err != nil {
-		t.Fatalf("an error was not expected: %s", err)
-	}
 }
